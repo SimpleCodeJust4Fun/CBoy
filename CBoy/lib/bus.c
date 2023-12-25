0d3cb81148228efd786dc177a325450f79e95c81
SimpleCodeJust4Fun <tyliuty@outlook.com>
2023-12-25 17:03:58 +0800

    debug for part 05

--- CBoy/lib/bus.c
+++ CBoy/lib/bus.c
@@ -1,8 +1,12 @@
 #include <bus.h>
 #include <cart.h>
-// 0x0000	0x3FFF	16 KiB ROM bank 00	From cartridge, usually a fixed bank
-// 0x4000	0x7FFF	16 KiB ROM Bank 01~NN	From cartridge, switchable bank via mapper (if any)
-// 0x8000	0x9FFF	8 KiB Video RAM (VRAM)	In CGB mode, switchable bank 0/1
+#include <ram.h>
+
+// 0x0000 - 0x3FFF : ROM Bank 0
+// 0x4000 - 0x7FFF : ROM Bank 1 - Switchable
+// 0x8000 - 0x97FF : CHR RAM
+// 0x9800 - 0x9BFF : BG Map 1
+// 0x9C00 - 0x9FFF : BG Map 2
 // 0xA000	0xBFFF	8 KiB External RAM	From cartridge, switchable bank if any
 // 0xC000	0xCFFF	4 KiB Work RAM (WRAM)	
 // 0xD000	0xDFFF	4 KiB Work RAM (WRAM)	In CGB mode, switchable bank 1~7
@@ -18,19 +22,78 @@ u8 bus_read(u16 address) {
      if (address < 0x8000) {
         //ROM Data
         return cart_read(address);
+     } else if (address < 0xA000) {
+         // CHR/Map RAM
+         printf("UNSUPPORTED bus_read(%04X)\n", address);
+         NO_IMPL
+     } else if (address < 0xC000) {
+         // Cartridge RAM
+         return cart_read(address);
+     } else if (address < 0xE000) {
+         // WRAM (Working RAM)
+         return wram_read(address);
+     } else if (address < 0xFE00) {
+         // reserved ECHO RAM
+         return 0;
+     } else if (address < 0xFEA0) {
+         // Object attribute memory (OAM), with PPU
+         // TODO
+         printf("UNSUPPORTED bus_read(%04X)\n", address);
+         NO_IMPL
+     } else if (address < 0xFF00) {
+         // reserved unusable
+         return 0;
+     } else if (address < 0xFF80) {
+         // IO registers
+         // TODO
+         printf("UNSUPPORTED bus_read(%04X)\n", address);
+         NO_IMPL
+     } else if (address == 0xFFFF) {
+         // CPU ENABLE REGISTER
+         // TODO
+         printf("UNSUPPORTED bus_read(%04X)\n", address);
+         NO_IMPL
      }
-   printf("UNSIPPORTED bus_read(%04X)\n", address);
-   // NO_IMPL
-
+ 
+   return hram_read(address);
 }
 
 void bus_write(u16 address, u8 value) {
      if (address < 0x8000) {
         //ROM Data
-        return cart_write(address, value);
+        cart_write(address, value);
+     } else if (address < 0xA000) {
+         // CHR/Map RAM
+         printf("UNSUPPORTED bus_write(%04X)\n", address);
+         NO_IMPL
+     } else if (address < 0xC000) {
+         // Cartridge RAM
+         cart_write(address, value);
+     } else if (address < 0xE000) {
+         // WRAM (Working RAM)
+         wram_write(address, value);
+     } else if (address < 0xFE00) {
+         // reserved ECHO RAM
+     } else if (address < 0xFEA0) {
+         // Object attribute memory (OAM), with PPU
+         // TODO
+         printf("UNSUPPORTED bus_write(%04X)\n", address);
+         NO_IMPL
+     } else if (address < 0xFF00) {
+         // reserved unusable
+     } else if (address < 0xFF80) {
+         // IO registers
+         // TODO
+         printf("UNSUPPORTED bus_write(%04X)\n", address);
+         NO_IMPL
+     } else if (address == 0xFFFF) {
+         // CPU ENABLE REGISTER
+         // TODO
+         printf("UNSUPPORTED bus_write(%04X)\n", address);
+         NO_IMPL
      }
-   printf("UNSIPPORTED bus_write(%04X)\n", address);
-   // NO_IMPL
+ 
+   hram_write(address, value);
 }
 
 u8 bus_read16(u16 address) {
